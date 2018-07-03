f7146d46c3355af6e436140fb18cb1bf2b83fed0
Kevin R Bray <kevn.r.bray@boeing.com>
2018-07-03 13:41:29 -0700

    Refactor Sea Hag

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -693,6 +693,21 @@ int playAdventurer(struct gameState *state)
 }
 
 
+int playSeaHag(struct gameState *state)
+{
+  int currentPlayer = whoseTurn(state);
+
+  for (int i = 0; i < state->numPlayers; i++){
+    if (i != currentPlayer) {
+      state->discard[i][state->discardCount[i]] = state->deck[i][state->deckCount[i]--];			    state->deckCount[i]--;
+      state->discardCount[i]++;
+      state->deck[i][state->deckCount[i]--] = curse;//Top card now a curse
+    }
+  }
+
+  return 0;
+}
+
 
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
@@ -1203,14 +1218,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case sea_hag:
-      for (i = 0; i < state->numPlayers; i++){
-	if (i != currentPlayer){
-	  state->discard[i][state->discardCount[i]] = state->deck[i][state->deckCount[i]--];			    state->deckCount[i]--;
-	  state->discardCount[i]++;
-	  state->deck[i][state->deckCount[i]--] = curse;//Top card now a curse
-	}
-      }
-      return 0;
+      return playSeaHag(state);
 		
     case treasure_map:
       //search hand for another treasure_map
