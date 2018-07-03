447cd869516c5fa68325f82d7d8ba2bf5ec1f73b
Kevin R Bray <kevn.r.bray@boeing.com>
2018-07-03 15:27:49 -0700

    Add bug to Remodel (only allow gaining a card with the same value)

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -716,7 +716,7 @@ int playRemodel(struct gameState *state, int handPos, int choice1, int choice2)
 
   j = state->hand[currentPlayer][choice1];  //store card we will trash
 
-  if ((getCost(state->hand[currentPlayer][choice1]) + 2) > getCost(choice2))
+  if ((getCost(state->hand[currentPlayer][choice1]) + 0) > getCost(choice2))
   {
       return -1;
   }
