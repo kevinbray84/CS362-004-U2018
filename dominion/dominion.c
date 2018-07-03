2b48260b1ef382f708e8f4f50184992d56ac2a5c
Kevin R Bray <kevn.r.bray@boeing.com>
2018-07-03 15:30:36 -0700

    Fix white space in Sea Hag

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -699,7 +699,8 @@ int playSeaHag(struct gameState *state)
 
   for (int i = 0; i < state->numPlayers; i++){
     if (i != currentPlayer) {
-      state->discard[i][state->discardCount[i]] = state->deck[i][state->deckCount[i]--];			    state->deckCount[i]--;
+      state->discard[i][state->discardCount[i]] = state->deck[i][state->deckCount[i]--];			    
+      state->deckCount[i]--;
       state->discardCount[i]++;
       state->deck[i][state->deckCount[i]--] = curse;//Top card now a curse
     }
