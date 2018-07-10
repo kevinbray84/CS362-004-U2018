f6cd115c6fcb97d2c85ba29069f9fd9187cbac10
Kevin Bray <brayk@oregonstate.edu>
2018-07-10 16:08:22 -0700

    Add numHandCards test

--- projects/brayk/dominion/unittest2.c
+++ projects/brayk/dominion/unittest2.c
@@ -5,8 +5,34 @@
 #include <assert.h>
 #include "rngs.h"
 #include <stdlib.h>
+#include "interface.h"
+
 
 int main (int argc, char** argv)
 {
-    printf("test\n");
+    int initialHandCards = 5;
+    int seed = 1000;
+    int numPlayers = 2;
+	struct gameState G, testG;
+	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+			sea_hag, tribute, smithy, council_room};
+
+	// initialize a game state and player cards
+	initializeGame(numPlayers, k, seed, &G);
+
+	// ----------- TEST 1: Check hand counts during initial state --------------
+	printf("********* Testing numHandCards(): Expected == Actual? *********\n");
+    printf("TEST 1: Check all card counts during initial state\n");
+
+    printf("Num hand cards: %d == %d:     ", initialHandCards, numHandCards(&G));
+    assertTrue(initialHandCards == numHandCards(&G));
+
+	// ----------- TEST 2: Check after playing a smithy --------------
+
+    memcpy(&testG, &G, sizeof(struct gameState));
+    playSmithy(&testG, 1);
+    printf("After smithy: %d == %d:       ", numHandCards(&G) + 3, numHandCards(&testG));
+    assertTrue(numHandCards(&G) + 3 == numHandCards(&testG));
+
+    return 0;
 }
