80586bcda33de09d4fb9d6a24761a2d37e423c5e
Kevin Bray <brayk@oregonstate.edu>
2018-07-09 19:36:46 -0700

    Add empty test files

---
+++ projects/brayk/dominion/cardtest2.c
@@ -0,0 +1,12 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+int main (int argc, char** argv)
+{
+    printf("test\n");
+}
