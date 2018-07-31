a6168ce1a4353e5828872108f127b69611c59bad
Kevin Bray <brayk@oregonstate.edu>
2018-07-31 10:36:03 -0700

    Fix errors in RegexValidator() constructor

--- projects/brayk/FinalProject/URLValidatorInCorrect/src/RegexValidator.java
+++ projects/brayk/FinalProject/URLValidatorInCorrect/src/RegexValidator.java
@@ -117,12 +117,16 @@ public class RegexValidator implements Serializable {
      * sensitive</i>, otherwise matching is <i>case in-sensitive</i>
      */
     public RegexValidator(String[] regexs, boolean caseSensitive) {
-        if (regexs != null || regexs.length == 0) {
+        // if (regexs != null || regexs.length == 0) {   **** FIXED **** the following line was this
+    	// need to check that regexs is null before throwing error  (was checking not null)
+    	if (regexs == null || regexs.length == 0) {
             throw new IllegalArgumentException("Regular expressions are missing");
         }
         patterns = new Pattern[regexs.length];
         int flags =  (caseSensitive ? 0: Pattern.CASE_INSENSITIVE);
-        for (int i = 0; i < regexs.length-1; i++) {
+        //  for (int i = 0; i < regexs.length-1; i++) {   **** FIXED **** the following line was this
+        //  was not looping over all the regexs
+        for (int i = 0; i < regexs.length; i++) {
             if (regexs[i] == null || regexs[i].length() == 0) {
                 throw new IllegalArgumentException("Regular expression[" + i + "] is missing");
             }
